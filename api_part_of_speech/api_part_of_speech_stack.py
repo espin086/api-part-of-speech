dc473b8889d459370fbb32d3cdbad0520039f08f
EC2 Default User <ec2-user@ip-172-31-8-17.us-west-1.compute.internal>
2022-11-21 22:36:47 +0000

    finalized the part of speech model

--- api_part_of_speech/api_part_of_speech_stack.py
+++ api_part_of_speech/api_part_of_speech_stack.py
@@ -1,26 +1,40 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
-    Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-)
-
+Stack,
+aws_lambda as _lambda,
+aws_apigateway as apigw,
+aws_iam as iam)
 
 class ApiPartOfSpeechStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "ApiPartOfSpeechQueue",
-            visibility_timeout=Duration.seconds(300),
+        # create new IAM group and user
+        group = iam.Group(self, "ComprehendGroup")
+        user = iam.User(self, "ComprehendUser")
+        # add IAM user to the new group
+        user.add_to_group(group)
+        
+        
+        #Defines and AWS Lambda resource
+        my_lambda = _lambda.Function(
+        self, 'api-part-of-speech-lambda',
+        runtime=_lambda.Runtime.PYTHON_3_7,
+        code=_lambda.Code.from_asset('lambda'),
+        handler='api-part-of-speech.lambda_handler',
         )
-
-        topic = sns.Topic(
-            self, "ApiPartOfSpeechTopic"
+        
+        # add Comprehend permissions for Lambda function
+        statement = iam.PolicyStatement()
+        statement.add_actions("comprehend:*")
+        statement.add_actions("execute-api:*")
+        statement.add_resources("*")
+        my_lambda.add_to_role_policy(statement)
+        
+        
+        #defines api gateway
+        apigw.LambdaRestApi(
+        self, 'api-part-of-speech-api-gateway',
+        handler=my_lambda,
         )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
